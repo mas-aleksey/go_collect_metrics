efa9432ad1fb46556c75ce0871cf7ab1d43825ef
Alexey Matyunin <alexey.matyunin@vk.team>
2023-10-29 20:22:08 +0300

    fix test

--- internal/utils/report_test.go
+++ internal/utils/report_test.go
@@ -9,6 +9,6 @@ func TestNewJSONReport(t *testing.T) {
 	statistic := NewStatistic()
 	report := NewJSONReport(statistic, "123")
 
-	assert.GreaterOrEqual(t, len(report.Metrics), ReportCount)
-	assert.GreaterOrEqual(t, cap(report.Metrics), ReportCount)
+	assert.GreaterOrEqual(t, len(report.Metrics), 32)
+	assert.GreaterOrEqual(t, cap(report.Metrics), 32)
 }
